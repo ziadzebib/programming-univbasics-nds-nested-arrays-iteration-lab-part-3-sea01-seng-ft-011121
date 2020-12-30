bbdb5a7ca39f618b1687705f82943fefbfecc8d6
Ziad Zebib <zebibziad@gmail.com>
2020-12-30 02:05:48 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,4 +7,5 @@ def join_nested_strings(src)
   while element_index < src[row_index].count do
     if src[row_index][element_index].is_a? String
       string_array = src[row_index][element_index]
+    
 end
